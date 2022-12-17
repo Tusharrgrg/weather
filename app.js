085649ff954d7f27980f9bd87f45ebe256ffc422
Tushar agrawal <gargtushar495@gmail.com>
2022-12-17 22:51:56 +0530

    get data from api

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+
+
+
+const weatherURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=Jaipur&units=metric&appid=af0483d6155351656c86d794501f7acf";
+
+app.get('/', (req,res)=>{
+
+    
+    https.get(weatherURL , (response)=>{
+        response.on("data", (data)=>{
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconUrL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            res.write("<h1>The temperature of jaipur is :"+ temp + "</h1>");
+            res.send();
+            console.log(temp, icon);
+        });
+        // console.log(response);
+    });
+});
+
+
+app.listen(3000 , (err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log("server running on 3000 port");
+})
